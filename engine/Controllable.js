035219c6d5465abc0309019f2dd492809e5adb5c
Al <sergiojr.atom@gmail.com>
2018-12-31 13:02:20 -0200

    Animated things are now self-contained
    All we promised was done.

--- engine/Controllable.js
+++ engine/Controllable.js
@@ -1,28 +1,26 @@
-function Controllable(arg={},asyncloaded=true,parentclass)
+function Controllable(arg={})
 {
 	var self=this;
-	parentclass.call(this,arg,asyncloaded);
+	if(!"atomic" in this)throw "Controllable Class requires a pre-existing atomic structure.";
 	this.code=!arg.code?"wasd":arg.code;
 	this.anim=!arg.anim?["Idle","Walk","Jump"]:arg.anim;
-	this.press=new Array(this.code.length).fill(0);
+	this.press={};
+	for(var i=0;i<this.code.length;i++)
+	{
+		this.press[this.code[i]]=0;
+	}
 	this.on("keydown",((code)=>(e)=>{
-		for(var i=0;i<self.press.length;i++)
-		{
-			if(e.key==code[i]){return (self.press[i]=1,true);}
-		}
+		self.press[e.key]=1;
 	})(this.code));
 	this.on("keyup",((code)=>(e)=>{
-		for(var i=0;i<self.press.length;i++)
-		{
-			if(e.key==code[i]){return (self.press[i]=0,true);}
-		}
+		self.press[e.key]=0;
 	})(this.code));
 	this.lastcycle=0;
 	this.delay=!arg.delay?200:arg.delay;
 	this.action=!arg.action?function()
 	{
-		var vspeed=(self.press[0]-self.press[2])*10;
-		var hspeed=(self.press[1]-self.press[3])*10;
+		var vspeed=(self.press[self.code[0]]-self.press[self.code[2]])*10;
+		var hspeed=(self.press[self.code[1]]-self.press[self.code[3]])*10;
 		if(vspeed==hspeed&&hspeed==0&&self.sprite.animation._lastAnimationState.name!=self.anim[0])self.sprite.animation.play(self.anim[0]);
 		else if(!(vspeed==hspeed&&hspeed==0)&&self.sprite.animation._lastAnimationState.name!=self.anim[1]){self.sprite.animation.play(self.anim[1]);}
 		self.pos[1]-=vspeed;
--- pack/COW/Player.js
+++ pack/COW/Player.js
@@ -1,63 +1,64 @@
 function Player(arg={},asyncloaded=true)
 {
 	var self=this;
-	Controllable.call(this,arg,asyncloaded,Animatable);
+	Animatable.call(this,arg);
+	Controllable.call(this,arg);
 	this.anim=!arg.anim?["Idle","Walk","JumpStart","JumpEnd","WalkJumpStart","WalkJumpEnd","Attack"]:arg.anim;
 	this.falling=false;
 	this.vspeed=0;
 	this.hspeed=0;
 	this.action=!arg.action?function()
 	{
-		var hacc=(self.press[1]-self.press[3])*10;
+		var hacc=(self.press[self.code[1]]-self.press[self.code[3]])*10;
 		self.hspeed=hacc;
 		hspeed=self.hspeed;
-		if(self.falling&&(self.sprite.animation._lastAnimationState.name!=self.anim[4]||self.sprite.animation.isCompleted))
+		if(self.falling&&(self.view.animation._lastAnimationState.name!=self.anim[4]||self.view.animation.isCompleted))
 		{
 			self.vspeed-=1;
 		}
-		else if(self.press[0]==1&&!self.falling&&self.sprite.animation._lastAnimationState.name!=self.anim[2])
+		else if(self.press[self.code[0]]==1&&!self.falling&&self.view.animation._lastAnimationState.name!=self.anim[2])
 		{
-			if((self.press[3]+self.press[1])>0)
+			if((self.press[self.code[3]]+self.press[self.code[1]])>0)
 			{
-				self.sprite.animation.play(self.anim[4],1);
+				self.view.animation.play(self.anim[4],1);
 				self.vspeed=5;
 			}
-			else self.sprite.animation.play(self.anim[2],1);
-			self.sprite.animation.timeScale=2;
+			else self.view.animation.play(self.anim[2],1);
+			self.view.animation.timeScale=2;
 		}
-		else if(self.press[2]==1&&!self.falling&&self.sprite.animation._lastAnimationState.name!=self.anim[6])
+		else if(self.press[self.code[2]]==1&&!self.falling&&self.view.animation._lastAnimationState.name!=self.anim[6])
 		{
-			self.sprite.animation.play(self.anim[6],1);
+			self.view.animation.play(self.anim[6],1);
 		}
 		else
 		{
-			if(self.sprite.animation._lastAnimationState.name==self.anim[2]||self.sprite.animation._lastAnimationState.name==self.anim[4])
+			if(self.view.animation._lastAnimationState.name==self.anim[2]||self.view.animation._lastAnimationState.name==self.anim[4])
 			{
-				if(!self.sprite.animation.isCompleted){}
+				if(!self.view.animation.isCompleted){}
 				else
 				{
 					self.vspeed=15;
 				}
 			}
-			else if((self.sprite.animation._lastAnimationState.name==self.anim[3]||self.sprite.animation._lastAnimationState.name==self.anim[5])&&!self.sprite.animation.isCompleted){}
-			else if(self.sprite.animation._lastAnimationState.name!=self.anim[6]||self.sprite.animation.isCompleted)
+			else if((self.view.animation._lastAnimationState.name==self.anim[3]||self.view.animation._lastAnimationState.name==self.anim[5])&&!self.view.animation.isCompleted){}
+			else if(self.view.animation._lastAnimationState.name!=self.anim[6]||self.view.animation.isCompleted)
 			{
-				self.sprite.animation.timeScale=1;
-				if(hspeed==0&&self.sprite.animation._lastAnimationState.name!=self.anim[0])self.sprite.animation.play(self.anim[0]);
-				else if(!(hspeed==0)&&self.sprite.animation._lastAnimationState.name!=self.anim[1]){self.sprite.animation.play(self.anim[1]);}
+				self.view.animation.timeScale=1;
+				if(hspeed==0&&self.view.animation._lastAnimationState.name!=self.anim[0])self.view.animation.play(self.anim[0]);
+				else if(!(hspeed==0)&&self.view.animation._lastAnimationState.name!=self.anim[1]){self.view.animation.play(self.anim[1]);}
 			}
 		}
 		self.pos[1]-=self.vspeed;
 		if(self.pos[1]>window.innerHeight)
 		{
 			self.vspeed=0;
-			if(self.sprite.animation._lastAnimationState.name==self.anim[2])self.sprite.animation.play(self.anim[3],1);
-			if(self.sprite.animation._lastAnimationState.name==self.anim[4])self.sprite.animation.play(self.anim[5],1);
+			if(self.view.animation._lastAnimationState.name==self.anim[2])self.view.animation.play(self.anim[3],1);
+			if(self.view.animation._lastAnimationState.name==self.anim[4])self.view.animation.play(self.anim[5],1);
 			self.pos[1]=window.innerHeight;
 			self.falling=false;
 		}
 		else if(self.pos[1]<window.innerHeight)self.falling=true;
-		if(self.sprite.animation._lastAnimationState.name==self.anim[1]||self.sprite.animation._lastAnimationState.name==self.anim[4]||self.sprite.animation._lastAnimationState.name==self.anim[5])self.pos[0]-=hspeed;
+		if(self.view.animation._lastAnimationState.name==self.anim[1]||self.view.animation._lastAnimationState.name==self.anim[4]||self.view.animation._lastAnimationState.name==self.anim[5])self.pos[0]-=hspeed;
 		if(hspeed!=0)self.transform.scale._x=hspeed/10;
 	}:arg.action;
 }
